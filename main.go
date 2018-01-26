0b55cbe26304eb4bca6566260ec4c01ca96f86dd
BinHsu <pcpunkhades@hotmail.com>
2018-01-26 23:18:23 +0800

    added npipe to communicate

--- main.go
+++ main.go
@@ -1,7 +1,71 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+	"time"
+
+	"gopkg.in/natefinch/npipe.v2"
+)
+
+const NAMED_PIPE = "\\\\.\\pipe\\HelloEvent"
+
+func handleConnection(conn net.Conn) {
+	r := bufio.NewReader(conn)
+	msg, err := r.ReadString('\n')
+	if err != nil {
+		fmt.Println("received msg failed:", err.Error())
+		return
+	}
+	fmt.Println("received msg:", msg)
+}
 
 func main() {
-    fmt.Println("hello world")
+	fmt.Println("hello Event")
+
+	optNum := len(os.Args)
+	if 3 > optNum {
+		fmt.Println("Server")
+
+		go func() {
+			ln, err := npipe.Listen(NAMED_PIPE)
+			if err != nil {
+				fmt.Println("Listen named_pipe failed:", err.Error())
+				os.Exit(-1)
+			}
+
+			for {
+				conn, err := ln.Accept()
+				if err != nil {
+					fmt.Println("Accept named_pipe failed:", err.Error())
+					continue
+				}
+				go handleConnection(conn)
+			}
+		}()
+	} else {
+		fmt.Println("Client")
+
+		//option := os.Args[1]
+		msg := os.Args[2]
+
+		go func(msg string) {
+			conn, err := npipe.Dial(NAMED_PIPE)
+			if err != nil {
+				fmt.Println("Dial named_pipe failed:", err.Error())
+			}
+			if _, err := fmt.Fprintln(conn, msg); err != nil {
+				fmt.Println("write to named_pipe failed:", err.Error())
+			}
+			/*r := bufio.NewReader(conn)
+			  msg, err := r.ReadString('\n')
+			  if err != nil {
+			      // handle eror
+			  }
+			  fmt.Println(msg)*/
+		}(msg)
+	}
+	time.Sleep(100 * time.Second)
 }
